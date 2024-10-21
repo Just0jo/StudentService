10a8a5983f2ebafed611a9234a29a926f5a6da1b
Just0jo <G00407342@atu.ie>
2024-10-21 12:20:02 +0100

    product class

---
+++ src/main/java/ie/atu/studentsevice/Controller.java
@@ -0,0 +1,4 @@
+package ie.atu.studentsevice;
+
+public class Controller {
+}
--- src/main/java/ie/atu/studentsevice/Product.java
+++ src/main/java/ie/atu/studentsevice/Product.java
@@ -1,5 +1,22 @@
 package ie.atu.studentsevice;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Product {
+
+    private long studentId;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private long dateOfBirth;
 
 }
