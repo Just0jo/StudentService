da27f32be0186b7d689a34109735de3ad87f2410
Just0jo <G00407342@atu.ie>
2024-10-21 12:14:15 +0100

    creation

---
+++ src/main/java/ie/atu/studentsevice/Product.java
@@ -0,0 +1,5 @@
+package ie.atu.studentsevice;
+
+public class Product {
+
+}
---
+++ src/main/java/ie/atu/studentsevice/StudentSeviceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.studentsevice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentSeviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentSeviceApplication.class, args);
+    }
+
+}
